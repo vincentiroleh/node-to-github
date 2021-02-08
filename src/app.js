acfa736ef71a391abcf4ba59957ef2d89cd1fd9f
Vincent Iroleh <irolehiroleh@gmail.com>
2021-02-08 09:10:52 +0100

    add logger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express'),
+    logger = require('morgan'),
     app = express(),
     PORT = process.env.PORT || 4000,
     router = require('./books/books.route');
 
 // Middleware 
 app.use(express.json());
-app.use(router)
+app.use(router);
+app.use(logger('dev'));
 
 // Server
 app.listen(PORT, () => {
